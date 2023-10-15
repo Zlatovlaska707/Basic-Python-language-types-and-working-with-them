06ce033cdb98d25822a50057836571ba3bdfc382
Дмитрий <122204932+Zlatovlaska707@users.noreply.github.com>
2023-10-15 21:20:47 +0300

    Update Task_1.py
    Подправил код. Теперь идет поиск элемента со значением None по всему списку

--- Лабораторная 1/Task_1.py
+++ Лабораторная 1/Task_1.py
@@ -1,5 +1,10 @@
 numbers = [2, -93, -2, 8, None, -44, -1, -85, -14, 90, -22, -90, -100, -8, 38, -92, -45, 67, 53, 25]
 
 # TODO заменить значение пропущенного элемента средним арифметическим
-numbers[4] = (round((sum(numbers[:4])+sum(numbers[5:]))/(len(numbers)), 2))
+# Находим индекс элемента со значением None
+index_none = numbers.index(None)
+
+# Вычисляем среднее арифметическое значений, исключая None
+# Заменяем None на вычисленное среднее арифметическое
+numbers[index_none] = (round((sum(numbers[:index_none])+sum(numbers[index_none+1:]))/(len(numbers)), 2))
 print("Измененный список:", numbers)
